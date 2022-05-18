8746399edb379d6d5b77b0d4146cc9b2919cb1e6
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2022-05-18 14:50:54 -0300

    Animations, sobre-me e serviços completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,10 +12,42 @@ function onScroll() {
   }
 }
 
-function openMenu (){
+function openMenu() {
   document.body.classList.add('menu-expanded')
 }
 
-function closeMenu(){
+function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+// Por baixo dos panos do scrollReveal, o scroll ele é uma funçao que retorna um objeto, que tem chaves e dados ou seja propriedades, e metodos como o reveal, que é anonima
+// function ScrollReveal() {
+//   // return {
+//   //   reveal: function (selector, delay, duration, distance, easing) {}
+//   // }
+
+//   let options = {
+//     reveal: function (selector, delay, duration, distance, easing) {}
+//   }
+
+//   // Retornando o objeto de opçoes, a onde eu posso chamar as propriedades e metodos
+//   return options
+// }
+
+// let options = ScrollReveal().reveal('.js-reveal', 0, 1000, '50px', 'ease-in-out');
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card,
+  #about,
+  #about header,
+  #about .content`)
